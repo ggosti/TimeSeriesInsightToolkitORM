c96fe46f1b48ab6a60a08f442b829bd6e0c324bb
Giorgio G <giogosti@gmail.com>
2024-09-17 08:55:10 +0200

    did stuff

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
+from flask import render_template
 import connexion
 from db import init_db
+from models import Step
+
+from db import SessionLocal
 
 # Create the application instance
 app = connexion.App(__name__, specification_dir='./swagger/')
+#app = config.connex_app
 
 # Read the swagger.yml file to configure the endpoints
 app.add_api('swagger.yml')
+
+@app.route("/")
+def home():
+    session = SessionLocal()
+    steps = session.query(Step).all()
+    return render_template("home.html", steps=steps)
 
 if __name__ == '__main__':
     # Initialize the database
